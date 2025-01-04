9a53d70d0e68200f10b8cd086ba8d2207c055fb4
nrohitydv <nrohityadavv@gmail.com>
2025-01-04 22:04:54 +0545

    devlop first page(sign in page)

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,48 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { ScrollView, Image, View, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 const SignIn = () => {
+  const hanldeLogIn = () => {
+    // TODO: Implement Google Sign-In
+  };
   return (
-    <View>
-      <Text>Sign in</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome to Restate
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Get You Closer to {"\n"}{" "}
+            <Text className="text-primary-300">Your Ideal Home</Text>{" "}
+          </Text>
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to Restate with Google
+          </Text>
+          <TouchableOpacity
+            onPress={hanldeLogIn}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
