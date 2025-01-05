e1443073bc65f8a2d096f94de427b9b07b30baf3
nrohitydv <nrohityadavv@gmail.com>
2025-01-05 23:59:09 +0545

    added tabs to navigate through different pages

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,19 +1,84 @@
-import { useGlobalContext } from "@/lib/global-provider";
-import { Redirect, Slot } from "expo-router";
-import { ActivityIndicator } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import icons from "@/constants/icons";
 
-export default function AppLayout() {
-  const { isLogged, loading } = useGlobalContext();
-  if (loading) {
-    return (
-      <SafeAreaView className="bg-white h-full flex justify-center items-center">
-        <ActivityIndicator className="text-primary-300" size="large" />
-      </SafeAreaView>
-    );
-  }
-  if (!isLogged) {
-    return <Redirect href="/sign-in" />;
-  }
-  return <Slot />;
-}
+const TabIcon = ({
+  focused,
+  icon,
+  title,
+}: {
+  focused: boolean;
+  icon: any;
+  title: string;
+}) => {
+  return (
+    <View className="flex-1 mt-3 flex flex-col items-center">
+      <Image
+        source={icon}
+        tintColor={focused ? "#0061ff" : "#666876"}
+        resizeMode="contain"
+        className="size-6"
+      />
+      <Text
+        className={`${
+          focused
+            ? "text-primary-300 font-rubik-medium "
+            : "text-black-200 font-rubik"
+        } text-xs w-full text-center mt-1`}
+      >
+        {title}
+      </Text>
+    </View>
+  );
+};
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "white",
+          position: "absolute",
+          borderTopColor: "#0061FF1A",
+          borderTopWidth: 1,
+          minHeight: 70,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} title="Explore" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.person} title="Profile" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
